2f01e4e441396069954b5d1a1c8431e3fd0cd072
smehus <mehus.scott@gmail.com>
2014-03-23 14:31:00 -0400

    Initial Commit

---
+++ life-grade/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  life-grade
+//
+//  Created by scott mehus on 3/23/14.
+//  Copyright (c) 2014 scott mehus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ life-gradeTests/life_gradeTests.m
@@ -0,0 +1,34 @@
+//
+//  life_gradeTests.m
+//  life-gradeTests
+//
+//  Created by scott mehus on 3/23/14.
+//  Copyright (c) 2014 scott mehus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface life_gradeTests : XCTestCase
+
+@end
+
+@implementation life_gradeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
