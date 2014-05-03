edc89b2323c415112b403e021d97a0b0dea491c3
smehus <mehus.scott@gmail.com>
2014-05-03 00:24:47 -0400

    done for za night - time for ESO

--- life-grade/OpeningViewController.m
+++ life-grade/OpeningViewController.m
@@ -9,6 +9,7 @@
 #import "OpeningViewController.h"
 #import "SWRevealViewController.h"
 #import "DesiredGradeViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 
 @interface OpeningViewController () <UIScrollViewDelegate>
@@ -29,6 +30,9 @@
 @implementation OpeningViewController {
     
     int currentPage;
+    CGFloat navBarHeight;
+    CGFloat viewHeight;
+    CGSize viewSize;
 }
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -43,22 +47,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    
+
     UIColor *barColour = GREEN_COLOR;
     self.navigationController.navigationBar.barTintColor = barColour;
     
-    
+    navBarHeight = self.navigationController.navigationBar.frame.size.height + 20;
+    viewHeight = self.view.frame.size.height - navBarHeight;
     self.edgesForExtendedLayout = UIRectEdgeNone;
-
-    CGFloat navBarHeight = 44 + 20;
-    NSLog(@"begin %f", self.view.frame.origin.x);
-    CGSize viewSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height - navBarHeight);
+    viewSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height - navBarHeight);
     
     UIImage *bgImage = [UIImage imageNamed:@"Lined-Paper-"];
     UIImageView *bg = [[UIImageView alloc] initWithImage:bgImage];
-//    bg.frame = CGRectMake(-5, navBarHeight, self.view.frame.size.height, viewSize.height);
-    
+    bg.frame = CGRectMake(self.view.frame.origin.x, 0, self.view.frame.size.width, self.view.frame.size.height);
     [self.view addSubview:bg];
    
       self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width *3, viewSize.height);
@@ -67,10 +67,43 @@
     self.myRevealController = [self revealViewController];
     [self.view addGestureRecognizer:self.myRevealController.panGestureRecognizer];
     
+    [self setUpPageOne];
+    [self setUpPageTwo];
+    [self setUpPageThree];
     
-    self.pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(20, self.view.frame.size.height - 50, self.view.frame.size.width - 40, 20)];
-    [self.view addSubview:self.pageControl];
+ 
+    self.pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(20, viewHeight - 50, self.view.frame.size.width - 40, 20)];
+    self.pageControl.currentPage = 0;
+    self.pageControl.numberOfPages = 3;
+    self.pageControl.currentPageIndicatorTintColor = GREEN_COLOR;
+    self.pageControl.pageIndicatorTintColor = GREY_COLOR;
 
+    
+   
+   
+
+    [self.view addSubview:self.pageControl];
+    [self.view addSubview:self.scrollView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)openGradeController {
+    
+    DesiredGradeViewController *desiredController = [[DesiredGradeViewController alloc] init];
+    [self.myRevealController setFrontViewController:desiredController];
+    
+    
+}
+
+- (void)setUpPageOne {
+    
+    
+    
     self.scrollView.backgroundColor = [UIColor clearColor];
     self.LifeLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 50, 150, 50)];
     self.LifeLabel.textColor = GREY_COLOR;
@@ -89,43 +122,62 @@
                                        self.LifeLabel.frame.size.height + 60, 320, gradeSize);
     
     
+    [self.scrollView addSubview:self.LifeLabel];
+    [self.scrollView addSubview:self.GradeLabel];
+}
 
-    self.stepOne = [[UILabel alloc] initWithFrame:CGRectMake(350, 50, 150, 50)];
+- (void)setUpPageTwo {
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(viewSize.width, 0, viewSize.width, viewHeight)];
+    view.backgroundColor = [UIColor clearColor];
+
+    
+    
+    self.stepOne = [[UILabel alloc] initWithFrame:CGRectMake(30, 50, 150, 50)];
     self.stepOne.text = @"Step One";
     self.stepOne.textColor = GREY_COLOR;
     
     
-    self.startButton = [[UIButton alloc] initWithFrame:CGRectMake(640, 375, self.view.frame.size.width, 50)];
+    UIImage *arrowImg = [UIImage imageNamed:@"blue-arrow--"];
+    UIImageView *firstArrow = [[UIImageView alloc] initWithImage:arrowImg];
+    firstArrow.frame = CGRectMake(20, 100, arrowImg.size.width, arrowImg.size.height);
+    
+    
+    UIImageView *secondArrow = [[UIImageView alloc] initWithImage:arrowImg];
+    firstArrow.frame = CGRectMake(20, 200, arrowImg.size.width, arrowImg.size.height);
+    
+   
+//    UIImageView *threeArrow = [[UIImageView alloc] initWithImage:arrowImg];
+//    firstArrow.frame = CGRectMake(20, 300, arrowImg.size.width, arrowImg.size.height);
+//    
+    
+    
+    
+    
+    
+    [view addSubview:firstArrow];
+    [view addSubview:secondArrow];
+//    [view addSubview:threeArrow];
+    [view addSubview:self.stepOne];
+    [self.scrollView addSubview:view];
+}
+
+- (void)setUpPageThree {
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(viewSize.width * 2, 0, viewSize.width, viewHeight)];
+    view.backgroundColor = [UIColor clearColor];
+    
+    self.startButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 375, self.view.frame.size.width, 50)];
     [self.startButton setBackgroundColor:[UIColor colorWithRed:176.0/255.0 green:226.0/255.0 blue:0.0/255.0 alpha:1.0f]];
     [self.startButton addTarget:self action:@selector(openGradeController) forControlEvents:UIControlEventTouchUpInside];
     [self.startButton setTitle:@"Start Grading" forState:UIControlStateNormal];
+    
 
     
     
-    
-    
-    [self.scrollView addSubview:self.startButton];
-    [self.scrollView addSubview:self.stepOne];
-    [self.scrollView addSubview:self.LifeLabel];
-    [self.scrollView addSubview:self.GradeLabel];
-    
-    
-    
-    [self.view addSubview:self.scrollView];
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void)openGradeController {
-    
-    DesiredGradeViewController *desiredController = [[DesiredGradeViewController alloc] init];
-    [self.myRevealController setFrontViewController:desiredController];
-    
-    
+     [view addSubview:self.startButton];
+    [self.scrollView addSubview:view];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
