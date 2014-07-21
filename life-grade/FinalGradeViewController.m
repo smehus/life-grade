9252aeac974888dc4668d7d501027127259d38bc
scottmehus <scottmehus@scotts-MacBook-Air.local>
2014-07-21 08:13:34 -0400

    accordion set up

--- life-grade/FinalGradeViewController.m
+++ life-grade/FinalGradeViewController.m
@@ -39,6 +39,9 @@
 
 - (void)viewDidLoad
 {
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
     [super viewDidLoad];
     [self performFetch];
     self.view.backgroundColor = [UIColor whiteColor];
@@ -106,7 +109,39 @@
     self.emTableView = [[EMAccordionTableViewController alloc] initWithTable:self.tableView withAnimationType:EMAnimationTypeBounce];
     [self.emTableView setDelegate:self];
     
+   NSMutableArray * dataSection01 = [[NSMutableArray alloc] initWithObjects:@"Dog", @"Cat", @"Pig", nil];
+    NSMutableArray *  dataSection02 = [[NSMutableArray alloc] initWithObjects:@"Federer", @"Nadal", nil];
     
+    UIColor *sectionsColor = [UIColor colorWithRed:62.0f/255.0f green:119.0f/255.0f blue:190.0f/255.0f alpha:1.0f];
+    UIColor *sectionTitleColor = [UIColor whiteColor];
+    UIFont *sectionTitleFont = [UIFont fontWithName:@"Futura" size:24.0f];
+    
+    EMAccordionSection *section01 = [[EMAccordionSection alloc] init];
+    [section01 setBackgroundColor:sectionsColor];
+    [section01 setItems:dataSection01];
+    [section01 setTitle:@"Animals"];
+    [section01 setTitleFont:sectionTitleFont];
+    [section01 setTitleColor:sectionTitleColor];
+    [self.emTableView addAccordionSection:section01];
+    
+    EMAccordionSection *section02 = [[EMAccordionSection alloc] init];
+    [section02 setBackgroundColor:sectionsColor];
+    [section02 setItems:dataSection02];
+    [section02 setTitle:@"Tennis players"];
+    [section02 setTitleColor:sectionTitleColor];
+    [section01 setTitleFont:sectionTitleFont];
+    [self.emTableView addAccordionSection:section02];
+    
+    EMAccordionSection *section03 = [[EMAccordionSection alloc] init];
+    [section03 setBackgroundColor:sectionsColor];
+    [section03 setTitle:@"Buh!"];
+    [section03 setTitleColor:sectionTitleColor];
+    [section01 setTitleFont:sectionTitleFont];
+    [self.emTableView addAccordionSection:section03];
+    
+    NSMutableArray *sections = [[NSArray alloc] initWithObjects:section01, section02, section03, nil];
+    
+    [self.view addSubview:self.emTableView.tableView];
     
     
 }
@@ -115,6 +150,38 @@
 {
     [super didReceiveMemoryWarning];
 
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return 44;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    
+    
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSLog(@"BALLZ");
 }
 
 
