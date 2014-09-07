27a9cec6e9a82f17a611308cdbfed9ffeea4a159
scottmehus <scottmehus@scotts-MacBook-Air.local>
2014-09-07 12:27:17 -0400

    rando

--- life-grade/AboutViewController.m
+++ life-grade/AboutViewController.m
@@ -11,8 +11,13 @@
 
 @interface AboutViewController ()
 @property (nonatomic, weak) IBOutlet UIBarButtonItem *revealButton;
+@property (nonatomic, strong) UIImageView *aboutImage;
+@property (nonatomic, strong) UIScrollView *scrollView;
 
 @end
+
+
+#define NAVBAR_HEIGHT 66
 
 @implementation AboutViewController
 
@@ -28,6 +33,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
     
 
     UIColor *barColour = GREEN_COLOR;
@@ -40,22 +46,48 @@
 //    [self.view addSubview:bg];
 //    [self.view sendSubviewToBack:bg];
     
-    self.view.backgroundColor = [UIColor purpleColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     UIBarButtonItem *barbut = [[UIBarButtonItem alloc] initWithTitle:@"Menu" style:UIBarButtonItemStyleBordered target:self action:@selector(revealToggle:)];
     [barbut setTintColor:[UIColor blackColor]];
     self.navigationItem.leftBarButtonItem = barbut;
     
     self.revealButton = barbut;
-    
-    
+    self.title = @"YouTime Coaching";
+
+    [self addImage];
+    [self addScrollView];
     [self.revealButton setTarget: self.revealViewController];
     [self.revealButton setAction: @selector( revealToggle: )];
     
-    
-    
-    
 
+}
+
+- (void)addImage{
+    
+    self.aboutImage = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 100, 200)];
+    UIImage *img = [UIImage imageNamed:@"jonsafag"];
+    self.aboutImage.image = img;
+
+    CGSize size = img.size;
+    self.aboutImage.frame = CGRectMake(10, 10, size.width*.5, size.height*.5);
+    
+    
+    
+    [self.view addSubview:self.aboutImage];
+    
+}
+
+- (void)addScrollView {
+    
+    NSString *aboutString = @"Meet Jonathan, your YouTime Coach! Thirty seconds of paralysis is all it took to force Jonathan into the world of Sport and Performance Psychology. Following a neck injury sustained in a senior year Varsity baseball game, Jonathan battled the mental and physical rigors of returning to play competitive baseball again. Overcoming the adversity, Jonathan was invited to pitch at a competitive Division I school, and this is where his passion for working with individuals on their ability to be resilient and thrive began. The YouTime Coach brings years of Life Coaching experience working with adolesence, adults, athletes, business executives, actors, and many other backgrounds throughout the country and internationally. Jonathan is passionate and genuine in his work with other individuals and does everything possible to see they succeed. Jonathan attended the University of Rhode Island to obtain a degree in Kinesiology and Psychology before coming up to Boston to earn his Master's Degree in Counseling and Sport Psychology from Boston University. In addition to his work as a Life Coach, Jonathan serves as a Consultant for a private psychiatric facility in Boston where he runs life coaching groups, is a 1st degree black belt, actively contributes to his blog Guidance for the Field of Life, and is currently developing a life coaching and happiness app. The YouTime Coach currently resides in the historic North End District of Boston and loves going out to new restaurants, trying new things in Boston, traveling with his wife and spending time with his dog Bipsy";
+    
+    UITextView *bioView = [[UITextView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.aboutImage.frame) + 5, self.view.frame.size.width, self.view.frame.size.height - CGRectGetMaxY(self.aboutImage.frame) - NAVBAR_HEIGHT)];
+    bioView.text = aboutString;
+    
+    
+    [self.view addSubview:bioView];
+    [self.view addSubview:self.scrollView];
 }
 
 - (void)didReceiveMemoryWarning
